e726e6179f8b74e5efdfbdbdef048bee50877836
Pedro Henrique Costa Lacerda <pedro.lacerda@qualisystem.local>
2023-10-24 11:50:26 -0300

    forEach

--- map_reduce_filter.js
+++ map_reduce_filter.js
@@ -1,15 +1,16 @@
 //retorne a soma do dobro de todos os pares
 // -> Filtrar os pares
-// -> Dobrar os valores 
+// -> Dobrar os valores
 // -> Reduzir (somar tudo)
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
 
-const filtrarNumeros = numeros.filter((numero)=>{
-if(numero % 2 === 0) return numero})
-console.log(filtrarNumeros)
+const filtrarNumeros = numeros.filter((numero) => {
+  if (numero % 2 === 0) return numero;
+});
+console.log(filtrarNumeros);
 
-const dobrarValor = filtrarNumeros.map((numero)=>numero *2)
-console.log(dobrarValor)
+const dobrarValor = filtrarNumeros.map((numero) => numero * 2);
+console.log(dobrarValor);
 
-const somarNumeros = dobrarValor.reduce((ac,valor)=> ac + valor)
-console.log(somarNumeros)
+const somarNumeros = dobrarValor.reduce((ac, valor) => ac + valor);
+console.log(somarNumeros);
--- reduce.js
+++ reduce.js
@@ -1,25 +1,25 @@
 //reduce usa os mesmo args do resto mas tambem com o acumulador no inicio(acumulador,valor,indice,array)
 //reduce reduz um array a um unico elemento
 const numeros = [5, 50, 80, 1, 2, 3, 5, 8, 7, 11, 15, 22, 27];
-const total = numeros.reduce((acumulador,valor,indice,array)=>{
-acumulador += valor
- console.log(acumulador,valor);
- return acumulador
-},0)//o 0 é o valor inicial do acumulador)
+const total = numeros.reduce((acumulador, valor, indice, array) => {
+  acumulador += valor;
+  console.log(acumulador, valor);
+  return acumulador;
+}, 0); //o 0 é o valor inicial do acumulador)
 
 //Retorne a pessoa mais velha
 
 const pessoas = [
-    { nome: "Luiz", idade: 62 },
-    { nome: "Maria", idade: 23 },
-    { nome: "Pedro", idade: 55 },
-    { nome: "Eduardo", idade: 19 },
-    { nome: "Rosana", idade: 32 },
-    { nome: "Wallace", idade: 47 },
-  ];
+  { nome: "Luiz", idade: 62 },
+  { nome: "Maria", idade: 23 },
+  { nome: "Pedro", idade: 55 },
+  { nome: "Eduardo", idade: 19 },
+  { nome: "Rosana", idade: 32 },
+  { nome: "Wallace", idade: 47 },
+];
 
-const maisVelha = pessoas.reduce((acumulador,valor)=>{
-    if(acumulador.idade >valor.idade)return acumulador
-return valor;
-})
-console.log(maisVelha)
+const maisVelha = pessoas.reduce((acumulador, valor) => {
+  if (acumulador.idade > valor.idade) return acumulador;
+  return valor;
+});
+console.log(maisVelha);
